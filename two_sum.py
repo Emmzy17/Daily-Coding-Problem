8df19d26a995c84aea780eea8468c53756c303df
Emmzy17 <emmanueladjerry@gmail.com>
2022-01-17 16:26:53 -0800

    coding problems

---
+++ two_sum.py
@@ -0,0 +1,13 @@
+from ctypes import pointer
+from operator import index
+
+
+def two_sum(arr, target):
+    dict_store = {}
+    for n in range(len(arr)):
+        for j in range(n+1, len(arr)):
+            if arr[n] + arr[j] == target:
+                return (arr[n], arr[j])
+         
+arr = [10,15,3,7]
+print(two_sum(arr, 17))
