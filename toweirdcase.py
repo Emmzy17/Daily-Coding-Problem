ae9cff13261af0063af01dcb76bbc1bc53e90b4b
Emmzy17 <emmanueladjerry@gmail.com>
2022-01-20 12:01:41 -0800

    daily coding

--- toweirdcase.py
+++ toweirdcase.py
@@ -1,10 +1,10 @@
 def toWeirdCase(string):
     string = list(string)
     for i in range(len(string)):
-        if i// 2 == 0:
+        if i% 2 == 0:
             string[i] = string[i].upper()
         else:
             string[i] = string[i].lower()
 
     return ''.join(string)
-print(toWeirdCase('biscuit'))
+print(toWeirdCase('come down here'))
